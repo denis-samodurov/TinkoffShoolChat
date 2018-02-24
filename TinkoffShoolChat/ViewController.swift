e9e296f5e224cb94481331f4e915687faf3aaa3c
denis-samodurov <samodurov.v.denis@gmail.com>
2018-02-25 02:00:11 +0300

    Initial Commit

---
+++ TinkoffShoolChat/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TinkoffShoolChat
+//
+//  Created by Denis Samodurov on 25/02/2018.
+//  Copyright © 2018 Denis Samodurov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
