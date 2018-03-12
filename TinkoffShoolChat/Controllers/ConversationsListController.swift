3949aa8dbeaa0d66d4bf8bfbfd9820fca0b08f20
denis-samodurov <samodurov.v.denis@gmail.com>
2018-03-13 01:40:55 +0300

    Setup a project structure

---
+++ TinkoffShoolChat/Controllers/ConversationsListController.swift
@@ -0,0 +1,21 @@
+//
+//  ConversationsListController.swift
+//  TinkoffShoolChat
+//
+//  Created by Denis Samodurov on 13/03/2018.
+//  Copyright © 2018 Denis Samodurov. All rights reserved.
+//
+
+import UIKit
+
+class ConversationsListController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+}
